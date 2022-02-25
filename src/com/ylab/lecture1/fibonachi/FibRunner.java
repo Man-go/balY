2a4a9db40fe5f4966f0294c846ec95c65bae4d7a
Severin <insmer@gmail.com>
2022-02-25 16:04:05 +0500

    add FibRunner for test fibonachi classes

---
+++ src/com/ylab/lecture1/fibonachi/FibRunner.java
@@ -0,0 +1,13 @@
+package com.ylab.lecture1.fibonachi;
+
+public class FibRunner {
+
+    public static void main(String[] args) {
+        System.out.println(FibRecursive.fib(10));
+        System.out.println(FibLoop.fib(90));
+        System.out.println(FibRecursiveCache.fib(90));
+        System.out.println(FibLoopMemoryReduce.fib(90));
+        System.out.println(Long.MAX_VALUE);
+        //9_223_372_036_854_775_807
+    }
+}
